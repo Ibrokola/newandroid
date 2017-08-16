b8ceab68e1a191e8dc121a7f93298d10dbbfb8e9
ibrobabs <babskolawole@gmail.com>
2017-08-16 11:58:42 -0600

    update

--- src/components/header.js
+++ src/components/header.js
@@ -21,9 +21,9 @@ const styles = {
         height: 50,
         paddingTop: 15,
         shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
+        shadowOffset: { width: 2, height: 4 },
         shadowOpacity: 0.2,
-        elevation: 2,
+        elevation: 3,
         position : 'relative'
     },
     textStyle: {
