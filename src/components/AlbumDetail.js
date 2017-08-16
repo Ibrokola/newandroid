216c89da93a3ba1bb73d0615b45119593070ce74
ibrobabs <babskolawole@gmail.com>
2017-08-16 15:54:29 -0600

    update

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+ 
+    return (
+        <Card>
+            <CardSection>
+                <View>
+                    <Image source={{ uri: thumbnail_image }} /> 
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>            
+        </Card>
+    );
+};
+
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    }
+};
+
+export default AlbumDetail;
